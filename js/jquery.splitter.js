7c1588f1d457097aa7ff2aba62d72cd31c1a3abe
Jakub Jankiewicz <jcubic@onet.pl>
2016-11-12 15:43:30 +0100

    Update version

--- js/jquery.splitter.js
+++ js/jquery.splitter.js
@@ -1,5 +1,5 @@
 /*!
- * JQuery Spliter Plugin version 0.23.0
+ * JQuery Spliter Plugin version 0.24.0
  * Copyright (C) 2010-2016 Jakub Jankiewicz <http://jcubic.pl>
  *
  * This program is free software: you can redistribute it and/or modify
